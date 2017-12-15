51d145506d0322530e1eaf9d7fcf32402471a2d4
zenolyth <31319479+zenolyth@users.noreply.github.com>
2017-12-14 23:40:45 -0800

    In Class Tests

---
+++ Testing/src/FlyingBirdRequirements.java
@@ -0,0 +1,11 @@
+
+public interface FlyingBirdRequirements {
+	
+	public void fly() {
+		
+	}
+	
+	public void ruffles() {
+		
+	}
+}
---
+++ Testing/src/Pigeon.java
@@ -0,0 +1,12 @@
+
+public class Pigeon extends Aminal implements FlyingBirdRequirements{
+	
+	public Pigeon() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Aminal Amelia = new Aminal();
+		Pigeon fluffy = (Pigeon) new Aminal();
+	}
+}
